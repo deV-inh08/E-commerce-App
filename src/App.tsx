86e3a324b564f3999f2ffff485039080ca294b79
deV-inh08 <vintd2004@gmail.com>
2024-09-17 16:58:49 +0700

    initalized project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes , Route} from 'react-router-dom'
+import './App.css'
+function App() {
+
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
+    </div> 
+  )
+}
+
+export default App
